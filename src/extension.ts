9a5a869880e9d66286fd6a819f54763c5cee1cd1
Dmitry Maslennikov <mrdaimor@gmail.com>
2021-05-20 13:02:02 +0300

    fixes, updates and smaller size

--- src/extension.ts
+++ src/extension.ts
@@ -35,7 +35,9 @@ export async function activate(extContext: ExtensionContext): Promise<IDriverExt
         extension.resourcesMap().set(`driver/${value}/connection-schema`, extContext.asAbsolutePath('connection.schema.json'));
         extension.resourcesMap().set(`driver/${value}/ui-schema`, extContext.asAbsolutePath('ui.schema.json'));
       });
-      await extension.client.sendRequest('ls/RegisterPlugin', { path: extContext.asAbsolutePath('out/ls/plugin.js') });
+      await extension.client.sendRequest("ls/RegisterPlugin", {
+        path: extContext.asAbsolutePath("dist/ls/plugin.js"),
+      });
     }
   };
   api.registerPlugin(plugin);
